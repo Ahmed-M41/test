c49fc56e642607b530f4890d92ec56051c5d0ada
Ahmed El-Motaz <amotaz@egyptpay.com>
2024-02-20 02:26:22 +0200

    Add Seed To books table

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,17 @@ namespace CodingWiki_DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Spider without duty", ISBN = "123ASD", Price = 10.99m },
+                new Book { BookId = 2, Title = "Fortune of time", ISBN = "231DES", Price = 11.99m });
+
+            var bookList = new Book[]
+            {
+                new Book { BookId = 3, Title = "Fake sunday", ISBN = "123ASD", Price = 20.99m },
+                new Book { BookId = 4, Title = "Cookie jar", ISBN = "AS241", Price = 21.99m },
+                new Book { BookId = 5, Title = "Cloudy forest", ISBN = "12D43", Price = 25.99m },
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
         }
 
         private string GetConnectionString()
---
+++ CodingWiki_DataAccess/Migrations/20240220002526_SeedBookTable.cs
@@ -0,0 +1,52 @@
+﻿#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class SeedBookTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123ASD", 10.99m, "Spider without duty" },
+                    { 2, "231DES", 11.99m, "Fortune of time" },
+                    { 3, "123ASD", 20.99m, "Fake sunday" },
+                    { 4, "AS241", 21.99m, "Cookie jar" },
+                    { 5, "12D43", 25.99m, "Cloudy forest" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+        }
+    }
+}
