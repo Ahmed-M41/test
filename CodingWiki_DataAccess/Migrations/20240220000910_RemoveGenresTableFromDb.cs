27cd4c36ea32f5ec132b48483fe24e62da2bc64c
Ahmed El-Motaz <amotaz@egyptpay.com>
2024-02-20 02:11:51 +0200

    Delete Genres table

---
+++ CodingWiki_DataAccess/Migrations/20240220000910_RemoveGenresTableFromDb.cs
@@ -0,0 +1,35 @@
+﻿#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class RemoveGenresTableFromDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false),
+                    GenreName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
